5fbe8307c2efeefe09c02661569f6ecbfe38ae84
bran259 <kiprop686@gmail.com>
2025-09-26 10:59:30 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(response => response.json()) // parse response body
+  .then(data => {
+    // append the new ID to the DOM
+    const body = document.querySelector("body");
+    body.innerHTML += `<p>User ID: ${data.id}</p>`;
+  })
+  .catch(error => {
+    // append the error message to the DOM
+    const body = document.querySelector("body");
+    body.innerHTML += `<p>Error: ${error.message}</p>`;
+  });
+}
+
+
